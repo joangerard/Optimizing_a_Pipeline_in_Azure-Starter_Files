50fa78284d64850d0ac48cd556ff1dd47a17a1dd
joangerard <joan.sebastian.gerard@gmail.com>
2021-12-08 02:17:53 +0000

    automl complete

--- .ipynb_checkpoints/train-checkpoint.py
+++ .ipynb_checkpoints/train-checkpoint.py
@@ -66,7 +66,7 @@ def main():
     run.log("Max iterations:", np.int(args.max_iter))
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
-
+    joblib.dump(value=model, filename="./outputs/model.joblib")
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
 
